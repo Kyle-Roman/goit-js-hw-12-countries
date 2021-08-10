3d1ce29fb0711984d189a322edb150bc8962dfb5
Kirill Romanenko <brs.group.com@gmail.com>
2021-08-10 23:58:54 +0300

    pending

--- src/index.js
+++ src/index.js
@@ -1,1 +1,44 @@
-import './sass/main.scss';
+import './styles/main.css'
+import API from './js/fetchCountries'
+import fetchCountries from './js/fetchCountries';
+import getRefs from './js/get_refs';
+// import { alert, notice, info, success, error } from '@pnotify/core';
+
+
+const refs = getRefs();
+
+refs.searchInput.addEventListener('keydown', onSearch);
+
+
+function onSearch(e) {
+    e.preventDefault();
+
+    const input = e.currentTarget;
+    const searchQuery = input.textContent;
+    console.log(searchQuery);
+
+    API.fetchCountries(searchQuery)
+        .then(renderCountriesList)
+        .catch(onError)
+        .finally(() => { input.reset() });
+
+    console.log(refs.searchInput.value);
+}
+
+
+function renderCountriesList(countries) {
+    const searchedCountries = countries.map(country => {
+        const countriesListItem = document.createElement('li');
+        countriesListItem.textContent = `${country}`;
+        return ingredientsListItem;
+    });
+    refs.countriesList.append(...searchedCountries);
+};
+
+function onError() {
+    const myError = error({
+        text: "Too many matches found. Please enter a more specific query!"
+    });
+    return myError;
+}
+// _.debounce(onSearch, 500)
---
+++ src/js/get_refs.js
@@ -0,0 +1,6 @@
+export default function getRefs() {
+    return {
+        searchInput: document.getElementById('input'),
+        countriesList: document.getElementById('countries_list'),
+    };
+}
