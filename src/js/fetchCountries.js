bd6d174860e9f129cd02e4d53589466991efe03c
Kirill Romanenko <brs.group.com@gmail.com>
2021-08-17 22:04:22 +0300

    added trim()

--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -2,7 +2,7 @@ const BASE_URL = 'https://restcountries.eu/rest/v2';
 
 function fetchCountries(name) {
     return fetch(`${BASE_URL}/name/${name}`)
-        .then((response) => { if (response.status === 200) { return response.json() } })
+        .then((response) => { if (response.ok) { return response.json() } })
 }
 
 
