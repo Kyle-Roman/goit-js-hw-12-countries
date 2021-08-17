e1b7abec61aababe50df3229edee44321eb2110c
Kirill Romanenko <brs.group.com@gmail.com>
2021-08-17 22:21:12 +0300

    added throw error

--- src/js/fetchCountries.js
+++ src/js/fetchCountries.js
@@ -2,7 +2,12 @@ const BASE_URL = 'https://restcountries.eu/rest/v2';
 
 function fetchCountries(name) {
     return fetch(`${BASE_URL}/name/${name}`)
-        .then((response) => { if (response.ok) { return response.json() } })
+        .then((response) => {
+            if (response.ok) {
+                return response.json()
+            }
+            throw new Error(console.log(`'error code' ${response.status}`))
+        })
 }
 
 
